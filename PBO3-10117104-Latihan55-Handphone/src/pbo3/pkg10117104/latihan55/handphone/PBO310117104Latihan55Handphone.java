3c7a95db68860cdd9d0f74201188eb700df2af85
AdityaSuheryana <43383601+AdityaSuheryana@users.noreply.github.com>
2018-10-30 01:08:34 +0700

    PBO3-10117104-Latihan55-Handphone

---
+++ PBO3-10117104-Latihan55-Handphone/src/pbo3/pkg10117104/latihan55/handphone/PBO310117104Latihan55Handphone.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan55.handphone;
+
+/**
+ *
+ * @author 
+ * Nama : Aditya Suheryana
+ * Kelas : IF-3/PBO3
+ * Nim : 10117104
+ * Deskripsi Program : Menampilkan jenis2 Handphone sesuai dengan subclassnya
+ */
+public class PBO310117104Latihan55Handphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Handphone hp = new Handphone( " " , " " , " " , 0 );
+        hp.displayProduct ();
+    }
+    
+}
